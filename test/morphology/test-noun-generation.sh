42fe36327faf55901031113068c3596a89603eeb
Sjur N Moshagen <sjurnm@mac.com>
2012-08-29 21:47:38 +0000

    Added .txt to all text files produced by the test. This will make it easy to ignore them, and to clean them. Also corrected the expression for reporting fail or not for the test to make.

--- test/morphology/test-noun-generation.sh
+++ test/morphology/test-noun-generation.sh
@@ -8,10 +8,10 @@
 grep ";" ${srcdir}/../../src/morphology/stems/nouns.lexc | grep -v "^\!" | \
 	egrep -v '(CmpN/Only|+Gen+|+Der+| R )' | sed 's/% /€/g' | sed 's/%:/¢/g' | \
 	tr ":+" " " | cut -d " " -f1 | tr -d "#" | tr "€" " " | tr "¢" ":" | \
-	sort -u > nouns
+	sort -u > nouns.txt
 # extract compounding lemmas:
 grep ";" ${srcdir}/../../src/morphology/stems/nouns.lexc | grep -v "^\!" | \
-	grep ' R '| tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > Rnouns
+	grep ' R '| tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > Rnouns.txt
 
 ###### Start testing: #######
 transducer_found=0
@@ -28,14 +28,14 @@ for f in  .xfst .hfst; do
 
 ###### Test non-comopunds: #######
 		# generate nouns in Singular, extract the resulting generated lemma, store it:
-		sed 's/$/+N+Sg+Nom/' nouns | $lookuptool ${srcdir}/../../src/generator.gt$f | \
-			cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analnouns 
+		sed 's/$/+N+Sg+Nom/' nouns.txt | $lookuptool ${srcdir}/../../src/generator.gt$f | \
+			cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analnouns.txt 
 		# Generate nouns, extract those that do not generate in singular,
 		# generate the rest in plural:
-		sed 's/$/+N+Sg+Nom/' nouns | $lookuptool ${srcdir}/../../src/generator.gt$f | \
+		sed 's/$/+N+Sg+Nom/' nouns.txt | $lookuptool ${srcdir}/../../src/generator.gt$f | \
 			cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Pl+Nom/' | \
 			$lookuptool ${srcdir}/../../src/generator.gt$f | cut -f2 | \
-			grep -v "^$" >> analnouns 
+			grep -v "^$" >> analnouns.txt 
 
 ###### Test comopunds: #######
 		# Test nouns that only can generate as compounds:
@@ -45,25 +45,25 @@ for f in  .xfst .hfst; do
 			sed 's/CmpN\/SgN$/Cmp#gåetie+N+Sg+Nom/' | \
 			sed 's/CmpN\/PlG$/Cmp#gåetie+N+Sg+Nom/' | \
 			$lookuptool ${srcdir}/../../src/generator.gt$f | cut -f2 | \
-			grep "\-" | grep -v "\-.*\-" | cut -d "-" -f1 > Ranalnouns
+			grep "\-" | grep -v "\-.*\-" | cut -d "-" -f1 > Ranalnouns.txt
 		grep ";" ${srcdir}/../../src/morphology/stems/nouns.lexc | \
 			grep -v "^\!" | grep ' R '| cut -d ":" -f1 | sed 's/+Use\/Sub//' | \
 			sed 's/+Dial\/-SW//' | sed 's/+Dial\/-NOR//' | sed 's/+CmpN\/Pref//' | \
 			sed 's/CmpN\/SgN$/Cmp#gåetie+N+Sg+Nom/' | \
 			sed 's/CmpN\/PlG$/Cmp#gåetie+N+Sg+Nom/' | \
 			$lookuptool ${srcdir}/../../src/generator.gt$f | \
-			cut -f2 | grep "\-" | grep "\-.*\-" | cut -d "-" -f1,2 >> Ranalnouns
+			cut -f2 | grep "\-" | grep "\-.*\-" | cut -d "-" -f1,2 >> Ranalnouns.txt
 
 ###### Collect results: #######
 		# Sort and compare original input with resulting output - the diff is stored and opened in SEE:
-		sort -u -o analnouns analnouns 
-		sort -u -o Ranalnouns Ranalnouns 
-		comm -23 nouns analnouns > missingnounLemmas.txt
-		comm -23 Rnouns Ranalnouns >> missingnounLemmas.txt
+		sort -u -o analnouns.txt analnouns.txt 
+		sort -u -o Ranalnouns.txt Ranalnouns.txt 
+		comm -23 nouns.txt analnouns.txt > missingnounLemmas.txt
+		comm -23 Rnouns.txt Ranalnouns.txt >> missingnounLemmas.txt
 		open -a SubEthaEdit missingnounLemmas.txt
 
 		# if at least one word is found, the test failed:
-		if [ `wc -w missingnounLemmas.txt` -gt 0 ]; then
+		if [ `wc -w missingnounLemmas.txt | tr -s ' ' | cut -d' ' -f2` -gt 0 ]; then
 		    exit 1
 		fi
 	fi
