f224c1fc1dc87420fe38c0632ab343bfd62be78d
Trond Trosterud <trond.trosterud@uit.no>
2012-08-29 09:03:32 +0000

    This file was just copied from the old structure, and had old filenames.
    adj-sma-lex.txt was replaced by 
    
    $GTHOME/newinfra/langs/sma/src/morphology/stems/adjectives.lexc 
    
    and 
    
    ../bin/isma.fst
    
    was replaced by
    
    $GTHOME/newinfra/langs/sma/src/generator.gt.xfst

--- test/morphology/testadj.sh
+++ test/morphology/testadj.sh
@@ -1,13 +1,13 @@
-# Dette skriptet tester at nesten alle lemmaene i adj-sma-lex.txt kan genereres. De som ikke kan genereres, kopieres til missingadjLemmas.txt
+# Dette skriptet tester at nesten alle lemmaene i $GTHOME/newinfra/langs/sma/src/morphology/stems/adjectives.lexc  kan genereres. De som ikke kan genereres, kopieres til missingadjLemmas.txt
 
-grep ";" adj-sma-lex.txt | grep -v "^\!" | egrep -v "(PRED|CASE| MES|EVTEBE)" | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > adjs
-cat adjs | sed 's/$/+A+Attr/' | $LOOKUP $GTHOME/gt/sma/bin/isma.fst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u > analadjs 
-grep ";" adj-sma-lex.txt | grep -v "^\!" | egrep "(PRED|CASE)" | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > predadjs
-cat predadjs | sed 's/$/+A+Sg+Nom/' | $LOOKUP $GTHOME/gt/sma/bin/isma.fst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
-grep ";" adj-sma-lex.txt | grep -v "^\!" | grep " MES" | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > superladjs
-cat superladjs | sed 's/$/+A+Superl+Sg+Nom/' | $LOOKUP $GTHOME/gt/sma/bin/isma.fst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
-grep ";" adj-sma-lex.txt | grep -v "^\!" | grep "EVTEBE" | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > compladjs
-cat compladjs | sed 's/$/+A+Comp+Sg+Nom/' | $LOOKUP $GTHOME/gt/sma/bin/isma.fst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
+grep ";" $GTHOME/newinfra/langs/sma/src/morphology/stems/adjectives.lexc  | grep -v "^\!" | egrep -v "(PRED|CASE| MES|EVTEBE)" | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > adjs
+cat adjs | sed 's/$/+A+Attr/' | $LOOKUP $GTHOME/newinfra/langs/sma/src/generator.gt.xfst  | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u > analadjs 
+grep ";" $GTHOME/newinfra/langs/sma/src/morphology/stems/adjectives.lexc  | grep -v "^\!" | egrep "(PRED|CASE)" | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > predadjs
+cat predadjs | sed 's/$/+A+Sg+Nom/' | $LOOKUP $GTHOME/newinfra/langs/sma/src/generator.gt.xfst  | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
+grep ";" $GTHOME/newinfra/langs/sma/src/morphology/stems/adjectives.lexc  | grep -v "^\!" | grep " MES" | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > superladjs
+cat superladjs | sed 's/$/+A+Superl+Sg+Nom/' | $LOOKUP $GTHOME/newinfra/langs/sma/src/generator.gt.xfst  | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
+grep ";" $GTHOME/newinfra/langs/sma/src/morphology/stems/adjectives.lexc  | grep -v "^\!" | grep "EVTEBE" | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > compladjs
+cat compladjs | sed 's/$/+A+Comp+Sg+Nom/' | $LOOKUP $GTHOME/newinfra/langs/sma/src/generator.gt.xfst  | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
 cat predadjs superladjs compladjs >> adjs
 sort -u -o adjs adjs 
 sort -u -o analadjs analadjs 
