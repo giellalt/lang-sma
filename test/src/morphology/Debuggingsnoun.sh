59f61cb135c2d99ed3a28467fa07d19e993c149c
Sjur N Moshagen <sjurnm@mac.com>
2015-02-12 06:13:25 +0000

    Moved fst's and generated source files into variables - makes it much easier for me to run these scripts, because they are in different locations on my system. The values as committed are as originally, so should work without change on other systems.

--- test/src/morphology/Debuggingsnoun.sh
+++ test/src/morphology/Debuggingsnoun.sh
@@ -1,5 +1,7 @@
 # Dette skriptet tester at nesten alle lemmaene i noun-sma-lex.txt kan genereres. De som ikke kan genereres, kopieres til missingnounLemmas.txt og blir så analysert for å hjelpe med debugging.
 
+generator=../../../src/generator-gt-norm.xfst
+
 # Hent alle lemmaer, bortsett fra hardkoda +Gen +Der og lemmaer som går til R
 grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|LexSub)" | egrep -v '(\+Gen\+|\+Der\+| R )' | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 |  tr "€" " " | tr "¢" ":" | sort -u > nouns
 
@@ -7,13 +9,13 @@ grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|LexSub)" | eg
 grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|LexSub)" | grep ' R '| tr ":+" " " | cut -d " " -f1 |  sort -u > Rnouns
 
 # Generer alle lemmaer med Sg+Nom
-cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst |  grep -v "\?" | cut -f2 | grep -v "^$" | sort -u > analnouns 
+cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $generator |  grep -v "\?" | cut -f2 | grep -v "^$" | sort -u > analnouns 
 
 # Generer lemmaer med Pl+Nom, de som ikke lar seg generere med Sg+Nom
-cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Pl+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst| cut -f2 | grep -v "^$" >> analnouns 
+cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $generator | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Pl+Nom/' | $LOOKUP $generator| cut -f2 | grep -v "^$" >> analnouns 
 
 # Generer R lemmaer som sammensatt med gåetie, fjern gåetie
-grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|LexSub)" | grep ' R '| cut -d ":" -f1 | cut -d "+" -f1 | sed 's/$/+N+Cmp#gåetie+N+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst| cut -f2 | sed 's/gåetie//' > Ranalnouns
+grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|LexSub)" | grep ' R '| cut -d ":" -f1 | cut -d "+" -f1 | sed 's/$/+N+Cmp#gåetie+N+Sg+Nom/' | $LOOKUP $generator| cut -f2 | sed 's/gåetie//' > Ranalnouns
 sort -u -o analnouns analnouns 
 sort -u -o Ranalnouns Ranalnouns 
 
@@ -23,4 +25,4 @@ comm -23 Rnouns Ranalnouns >> missingnounLemmas.txt
 
 rm *nouns
 # Generer missinglemmaer, for debugging
-cat missingnounLemmas.txt |sed 's/$/+N+Sg+Nom/'| $LOOKUP ../../../src/generator-gt-norm.xfst | see
+cat missingnounLemmas.txt |sed 's/$/+N+Sg+Nom/'| $LOOKUP $generator | see
