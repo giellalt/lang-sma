22e8bca03058f98dc3f5bb9a3fb73360297ceeb6
Sjur Moshagen <sjurnm@mac.com>
2015-02-12 06:13:25 +0000

    Moved fst's and generated source files into variables - makes it much easier for me to run these scripts, because they are in different locations on my system. The values as committed are as originally, so should work without change on other systems.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sma@107237 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/src/morphology/Debuggingstestproperlexc.sh
+++ test/src/morphology/Debuggingstestproperlexc.sh
@@ -1,28 +1,32 @@
 # Dette skriptet tester at alle lemmaene i propernoun-sma-lex.txt kan genereres. De som ikke kan genereres, kopieres til missingProperLemmas og blir så analysert for å hjelpe med debugging.
 
+generator=../../../src/generator-gt-norm.xfst
+analyser=../../../src/analyser-gt-norm.xfst
+soourcefile=../../../src/morphology/generated_files/smi-sma-propernouns.lexc
+
 # Hent alle lemmaer, bortsett fra hardkoda +Attr
-grep ";" ../../../src/morphology/generated_files/smi-sma-propernouns.lexc | egrep -v "(^\!|LexSub)" | egrep -v Attr | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr "€" " " | sort -u > props
+grep ";" $soourcefile | egrep -v "(^\!|LexSub)" | egrep -v Attr | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr "€" " " | sort -u > props
 
 # Generer alle lemmaer med Sg+Nom
-cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analprops 
+cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $generator | cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analprops 
 
 # Generer lemmaer med Pl+Nom, de som ikke lar seg generere med Sg+Nom
-cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/langs/sma/src/generator-gt-norm.xfst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/langs/sma/src/generator-gt-norm.xfst | grep -v '\?' | cut -f2 | grep -v "^$" >> analprops 
+cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $generator | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $generator | grep -v '\?' | cut -f2 | grep -v "^$" >> analprops 
 sort -u -o analprops analprops 
 
 # Sammenlikne lemmaer med genereringa
 comm -23 props analprops > missingProperLemmas
 
 # Hent alle lemmaer med hardkoda +Attr
-grep ";" ../../../src/morphology/generated_files/smi-sma-propernouns.lexc | egrep -v "(^\!|LexSub)" | grep Attr | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr "€" " " |  sort -u > attrprops
+grep ";" $soourcefile | egrep -v "(^\!|LexSub)" | grep Attr | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr "€" " " |  sort -u > attrprops
 
 # Generer lemmaene med +Attr
-cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/langs/sma/src/generator-gt-norm.xfst | grep -v "\?" | cut -f2  | grep -v "^$" | sort -u > analattrprops  
+cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $generator | grep -v "\?" | cut -f2  | grep -v "^$" | sort -u > analattrprops  
 
 # Sammenlikne lemmaer med genereringa
 comm -23 attrprops analattrprops >> missingProperLemmas
 
 # Analyser lemmaer som ikke lar seg generere, for debugging
-cat missingProperLemmas |$LOOKUP $GTHOME/langs/sma/src/analyser-gt-desc.xfst > analysermissingProperLemmas
+cat missingProperLemmas |$LOOKUP $analyser > analysermissingProperLemmas
 rm *props
 see analysermissingProperLemmas
