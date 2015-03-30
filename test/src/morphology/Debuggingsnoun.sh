8da01a4dae357025d65fe4985bc921730ab7ed98
Sjur N Moshagen <sjurnm@mac.com>
2015-03-30 09:21:33 +0000

    LexSub -> Err/Lex in testing and development scripts. Please check that the scripts are working as they should.

--- test/src/morphology/Debuggingsnoun.sh
+++ test/src/morphology/Debuggingsnoun.sh
@@ -3,10 +3,10 @@
 generator=../../../src/generator-gt-norm.xfst
 
 # Hent alle lemmaer, bortsett fra hardkoda +Gen +Der og lemmaer som går til R
-grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|LexSub)" | egrep -v '(\+Gen\+|\+Der\+| R )' | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 |  tr "€" " " | tr "¢" ":" | sort -u > nouns
+grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|\+Err\/Lex)" | egrep -v '(\+Gen\+|\+Der\+| R )' | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 |  tr "€" " " | tr "¢" ":" | sort -u > nouns
 
 # Hent alle lemmaer som går til R
-grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|LexSub)" | grep ' R '| tr ":+" " " | cut -d " " -f1 |  sort -u > Rnouns
+grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|\+Err\/Lex)" | grep ' R '| tr ":+" " " | cut -d " " -f1 |  sort -u > Rnouns
 
 # Generer alle lemmaer med Sg+Nom
 cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $generator |  grep -v "\?" | cut -f2 | grep -v "^$" | sort -u > analnouns 
@@ -15,7 +15,7 @@ cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $generator |  grep -v "\?" | cut -f2 
 cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $generator | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Pl+Nom/' | $LOOKUP $generator| cut -f2 | grep -v "^$" >> analnouns 
 
 # Generer R lemmaer som sammensatt med gåetie, fjern gåetie
-grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|LexSub)" | grep ' R '| cut -d ":" -f1 | cut -d "+" -f1 | sed 's/$/+N+Cmp#gåetie+N+Sg+Nom/' | $LOOKUP $generator| cut -f2 | sed 's/gåetie//' > Ranalnouns
+grep ";" ../../../src/morphology/stems/nouns.lexc | egrep -v "(^\!|\+Err\/Lex)" | grep ' R '| cut -d ":" -f1 | cut -d "+" -f1 | sed 's/$/+N+Cmp#gåetie+N+Sg+Nom/' | $LOOKUP $generator| cut -f2 | sed 's/gåetie//' > Ranalnouns
 sort -u -o analnouns analnouns 
 sort -u -o Ranalnouns Ranalnouns 
 
