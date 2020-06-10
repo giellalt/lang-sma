a0b795c01a5380af5b88c49c97029c1c5a459943
leneantonsen <lene.antonsen@uit.no>
2020-06-10 14:28:54 +0200

    oppdaterte testskripter til HFST og slik at man kan teste med alle propernouns

--- devtools/adj_minip.sh
+++ devtools/adj_minip.sh
@@ -14,7 +14,7 @@ GTHOME=$(echo $GTHOME)
 
 PATTERN=$1
 L_FILE="in.txt"
-cut -d '!' -f1 src/fst/stems/adjectives.lexc | egrep $PATTERN | cut -d ':' -f1>$L_FILE
+cut -d '!' -f1 src/fst/stems/adjectives.lexc | egrep $PATTERN | tr '+' ':' | cut -d ':' -f1>$L_FILE
 
 #P_FILE="test/data/testadjparadigm.txt"
 P_FILE="test/data/adj_paradigm.txt"
@@ -23,7 +23,7 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-   echo "${lemma}${form}" | $LOOKUP $GTHOME/langs/sma/src/generator-gt-norm.xfst
+   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-sma/src/generator-gt-norm.hfstol
  done
 done
 
