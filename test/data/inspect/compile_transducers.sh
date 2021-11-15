b99320e874d62922f36bc0738688dd70260dda9b
Sjur Moshagen <sjurnm@mac.com>
2021-11-15 14:42:19 +0200

    Add more regexes

--- test/data/inspect/compile_transducers.sh
+++ test/data/inspect/compile_transducers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # list of transducers used for inspecting typos and their corrections
 
-for typo in add1 one2double del1 double2one transpose transpose1 aab2abb repetition subst1 subst1_kb_next subst_accents subst_accents_sz insert_space capitalize
+for typo in add1 one2double del1 double2one transpose transpose1 aab2abb repetition subst1 subst1_kb_next subst_accents subst_accents_sz insert_space capitalize loansuffix vowels acrosuff
 do
   hfst-regexp2fst --format=openfst-tropical -v -S -i ${typo}.regex -o ${typo}.hfst
 done
