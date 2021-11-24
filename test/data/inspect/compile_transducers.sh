98a89202f5c98b678dae43a35b01d10e8431fadd
Sjur Moshagen <sjurnm@mac.com>
2021-11-24 09:28:07 +0200

    Add separate regex to handle errors in Sámi suffixes

--- test/data/inspect/compile_transducers.sh
+++ test/data/inspect/compile_transducers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # list of transducers used for inspecting typos and their corrections
 
-for typo in add1 one2double del1 double2one transpose transpose1 aab2abb repetition subst1 subst1_kb_next subst_accents subst_accents_sz insert_space capitalize loansuffix vowels acrosuff
+for typo in add1 one2double del1 double2one transpose transpose1 aab2abb repetition subst1 subst1_kb_next subst_accents subst_accents_sz insert_space capitalize loansuffix vowels acrosuff samisuffix
 do
   hfst-regexp2fst --format=openfst-tropical -v -S -i ${typo}.regex -o ${typo}.hfst
 done
@@ -11,8 +11,8 @@ printf "read regex [ @\"transpose1.hfst\" .o. [@\"transpose1.hfst\" | @\"double2
 save stack transpose1_plus.hfst\n\
 quit\n" | hfst-xfst
 
-# combine accented letter with a likely typo
-printf "read regex [ [@\"subst_accents.hfst\" | @\"subst_accents_sz.hfst\"] .o. [@\"transpose1.hfst\" | @\"double2one.hfst\" | @\"one2double.hfst\" ]]; \
+# combine accented letter and vowel errors with a likely typo
+printf "read regex [ [@\"subst_accents.hfst\" | @\"subst_accents_sz.hfst\" | @\"vowels.hfst\"] .o. [@\"transpose1.hfst\" | @\"double2one.hfst\" | @\"one2double.hfst\" ]]; \
 save stack accents_plus.hfst\n\
 quit\n" | hfst-xfst
 
