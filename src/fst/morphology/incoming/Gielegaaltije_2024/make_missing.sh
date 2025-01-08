7183e979d1951b70cf53deb0d0090da03fbd1ec9
Børre Gaup <albbas@gmail.com>
2025-01-08 17:47:40 +0100

    Make missinglist from dict files

---
+++ src/fst/morphology/incoming/Gielegaaltije_2024/make_missing.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+for i in A V N Phrase
+do
+    cat $i-smamul.xml|grep '<l '| cut -f2 -d'<' | cut -f2 -d'>' | hfst-lookup -q --time-cutoff=10 $GTLANGS/lang-sma/src/fst/analyser-gt-desc.hfstol | grep "?"|cut -f1 | rev | sort -u | rev | awk '{print $1":"$1" TODO ;"}' > $i-missing.txt
+done
