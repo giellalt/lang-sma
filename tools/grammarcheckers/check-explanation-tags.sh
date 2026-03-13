49b4a60c1f8225629a7e5c0b7dac23cbb48dac63
Trondtr <trond.trosterud@uit.no>
2026-03-13 11:11:46 +0100

    uniq

--- tools/grammarcheckers/check-explanation-tags.sh
+++ tools/grammarcheckers/check-explanation-tags.sh
@@ -2,20 +2,20 @@ echo "\n"
 echo "Script to check whether rule tags in grammarchecker.cg3 are declared in the errors* files\n"
 
 echo "List grammarchecker tags from grammarchecker file and save as xxg\n"
- cat grammarchecker.cg3 |grep "^LIST &"|cut -d" " -f2|tr -d "&"|sort > xxg
+ cat grammarchecker.cg3 |grep "^LIST &"|cut -d" " -f2|tr -d "&"|sort | uniq > xxg
 
 echo "List grammarchecker tags from native language ftl file and save as xxn\n"
- cat errors-sma.ftl |grep "^[a-z]"|cut -d" " -f1|sort > xxn
+ cat errors-sma.ftl |grep "^[a-z]"|cut -d" " -f1|sort | uniq > xxn
 
 echo "List grammarchecker tags from English ftl file and save as xxe\n"
- cat errors-en.ftl |grep "^[a-z]"|cut -d" " -f1|sort > xxe
+ cat errors-en.ftl |grep "^[a-z]"|cut -d" " -f1|sort | uniq > xxe
 
 
 echo "List grammarchecker tags from json file and save xxj\n"
- cat errors.json |grep '"id"'|cut -d'"' -f4|sort > xxj
+ cat errors.json |grep '"id"'|cut -d'"' -f4|sort | uniq > xxj
 
 echo "List grammarchecker tags from xml file and save xxx\n"
-cat errors.xml | grep "error id"|cut -d'"' -f2|sort > xxx
+cat errors.xml | grep "error id"|cut -d'"' -f2|sort | uniq > xxx
 
 echo "Grammarchecker tags missing in native ftl file (empty line if all tags are declared):\n"
  comm -23 xxg xxn
@@ -33,5 +33,5 @@ echo "Grammarchecker tags missing in the file errors.xml (empty line if all tags
  comm -23 xxg xxx
 echo "\n"
 
-echo "Delete auxiliary xx* files\n"
-rm -f xxe xxn xxg xxj xxx 
+#echo "Delete auxiliary xx* files\n"
+#rm -f xxe xxn xxg xxj xxx 
